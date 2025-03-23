2cd4650025aa62061f69cb67d6ec99223cee7803
GuriZenit <gurizenit@gmail.com>
2025-03-23 17:29:18 -0300

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,26 @@
+<script>
+    import { onMount } from 'svelte';
+
+    onMount(async () => {
+        console.log('layout');
+    });
+</script>
+
+<main>
+	<slot />
+</main>
+
+<style>
+	:global(body) {
+		background-color: #22212c;
+		color: #e6e6e6;
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+		align-items: center;
+		justify-content: center;
+		display: flex;
+		flex-direction: column;
+		min-height: 100vh;
+	}
+</style>
---
+++ src/routes/api/anime/list/+server.js
@@ -0,0 +1,56 @@
+import { json } from '@sveltejs/kit';
+
+export async function GET() {
+    try {
+        const response = await fetch('https://myanimelist.net/animelist/guritso/load.json?offset=0&order=5&status=7');
+        const data = await response.json();
+
+        return json(reduceJson(data));
+    } catch (error) {
+        console.error(error);
+        return json({ error: 'Failed to fetch anime list' }, { status: 500 });
+    }
+}
+
+function reduceJson(json) {
+    return json.map((anime) => {
+        return {
+            name: anime.anime_title,
+            episodes: anime.anime_num_episodes,
+            airing: anime.anime_airing_status === 1,
+            mediaType: anime.anime_media_type_string,
+            imageUrl: getImageUrl(anime.anime_image_path),
+            user: {
+                status: getStatus(anime.status),
+                score: anime.score,
+                episodes: anime.num_watched_episodes,
+                updated: getDate(anime.updated_at),
+            },
+        };
+    });
+}
+
+function getStatus(status) {
+    switch (status) {
+        case 1:
+            return 'watching';
+        case 2:
+            return 'completed';
+        case 3:
+            return 'on hold';
+        case 4:
+            return 'dropped';
+        case 6:
+            return 'plan to watch';
+        default:
+            return 'unknown';
+    }
+}
+
+function getImageUrl(url) {
+    return url.replace('/r/192x272/', '/').replace('.jpg', '.webp').split('?')[0];
+}
+
+function getDate(date) {
+    return new Date(date).toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' });
+}
