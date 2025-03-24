73efa1d518752afd2e94039c6e9ad0ff706f9d48
GuriZenit <gurizenit@gmail.com>
2025-03-24 09:49:34 -0300

    feat(anime): add AnimesLayout and Card components for displaying anime list

---
+++ src/lib/components/anime/AnimesLayout.svelte
@@ -0,0 +1,60 @@
+<script>
+    import Card from "./Card.svelte";
+
+    let { config } = $props();
+    let animes = $state([]);
+
+    $effect(async () => {
+        const res = await fetch("/api/anime/list");
+        const data = await res.json();
+
+        const filter = config.anime.filter;
+
+        animes = data.filter((card) => filter.includes(card.user.status)).slice(0, 30);
+        animes = [...animes, ...animes];
+    });
+</script>
+
+<div class="animes-layout">
+    <div class="animes-container">
+        {#each animes as anime}
+            <Card anime={anime} />
+        {/each}
+    </div>
+</div>
+
+<style>
+    .animes-layout {
+        border: 1px solid #3a3f41;
+        border-radius: 1rem;
+        padding: 1rem;
+        height: 240px;
+        overflow: hidden;
+    }
+    
+    .animes-container {
+        display: flex;
+        flex-direction: row;
+        gap: 1rem;
+        width: max-content;
+        animation: scroll 100s infinite linear;
+        animation-delay: 2s;
+    }
+
+    .animes-container::-webkit-scrollbar {
+        display: none;
+    }
+
+    .animes-container:hover {
+        animation-play-state: paused;
+    }
+
+    @keyframes scroll {
+        0% {
+            transform: translateX(0);
+        }
+        100% {
+            transform: translateX(-50%);
+        }
+    }
+</style>
---
+++ src/lib/components/anime/Card.svelte
@@ -0,0 +1,90 @@
+<script>
+    let { anime } = $props();
+</script>
+
+<article class="anime-card">
+    <div class="anime-card-image">
+        <img src={anime.imageUrl} alt={anime.name} />
+    </div>
+    <div class="anime-card-info">
+        <div>
+        <a href={anime.url} target="_blank" class="anime-card-title">{anime.name}</a>
+        <p class="anime-card-info-item">Episodes: <span>{anime.user.episodes}/{anime.episodes}</span></p>
+        <p class="anime-card-info-item">Status: <span>{anime.user.status}</span></p>
+        <p class="anime-card-info-item">Scored: <span>{anime.user.score ? anime.user.score : 'N/A'}</span></p>
+        </div>
+        <div>
+            <p class="anime-card-info-item-updated">{anime.user.updated}</p>
+        </div>
+    </div>
+</article>
+
+<style>
+    .anime-card {
+        width: 100%;
+        height: 238px;
+        border-radius: 1rem;
+        border: 1px solid #3a3f41;
+        display: flex;
+        flex-direction: row;
+    }
+
+    .anime-card:hover {
+        transform: scale(1.01);
+    }
+
+    .anime-card-image img {
+        border-radius: 1rem;
+        border-top-right-radius: 0;
+        border-bottom-right-radius: 0;
+        width: 170px;
+        height: 100%;
+        object-fit: cover;
+    }
+
+    .anime-card-info {
+        width: 170px;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        padding: 0.3rem;
+    }
+
+    .anime-card-title {
+        font-size: 1.05rem;
+        font-weight: 600;
+        font-weight: 400;
+        margin-bottom: 0.5rem;
+        display: -webkit-box;
+        -webkit-line-clamp: 2;
+        line-clamp: 2;
+        -webkit-box-orient: vertical;
+        overflow: hidden;
+        text-decoration: none;
+    }
+
+    .anime-card-info-item {
+        font-size: 0.95rem;
+        font-weight: 300;
+    }
+
+    .anime-card-info-item-updated {
+        font-size: 0.8rem;
+        font-weight: 300;
+        text-align: right;
+        margin-right: 0.2rem;
+    }
+
+    .anime-card-info-item span {
+        font-weight: 400;
+    }
+
+    .anime-card-title:hover  {
+        text-decoration: underline;
+        cursor: pointer;
+    }
+
+    p {
+        margin: 0;
+    }
+</style>
