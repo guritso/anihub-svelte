98e80e41c01faf018e2ff8e62df739819f539b4e
GuriZenit <gurizenit@gmail.com>
2025-03-24 01:32:17 -0300

    feat(api): add user profile and social data endpoint

---
+++ src/routes/api/config/+server.js
@@ -0,0 +1,33 @@
+import { json } from '@sveltejs/kit';
+
+export async function GET({ url }) {
+  return json({
+    data: {
+        user: {
+            name: "Guritso",
+            bio: "I like to code and watch animes",
+            avatar: "https://guritso.site/profile/picture/1224db7310c94b94b1217b6fa14d379db912c4394afb911cbc8bfcc0faf127be.webp"
+        },
+        social: [
+            {
+                name: "Github",
+                username: "Guritso",
+                url: "https://github.com/Guritso",
+            },
+            {
+                name: "MyAnimeList",
+                username: "Guritso",
+                url: "https://myanimelist.net/profile/Guritso",
+            },
+        ],
+        animes: {
+            filter: ["watching", "completed"],
+            rowSpeed: 0.6
+        },
+        repos: {
+            fork: false,
+            archived: false,
+        }
+    }
+  });
+}
