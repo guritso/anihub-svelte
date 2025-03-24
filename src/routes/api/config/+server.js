94201704d3d8504252386719f5cd18437099a8a7
GuriZenit <gurizenit@gmail.com>
2025-03-24 20:40:31 -0300

    feat(api): update user configuration with additional social links and exclude settings

--- src/routes/api/config/+server.js
+++ src/routes/api/config/+server.js
@@ -6,22 +6,21 @@ export async function GET({ url }) {
         user: {
             name: "Guritso",
             bio: "I like to code and watch animes",
-            avatar: "https://guritso.site/profile/picture/1224db7310c94b94b1217b6fa14d379db912c4394afb911cbc8bfcc0faf127be.webp"
+            avatar: "https://guritso.site/profile/picture/1224db7310c94b94b1217b6fa14d379db912c4394afb911cbc8bfcc0faf127be.webp",
+            myAnimeList: "guritso",
+            github: "guritso",
         },
         social: [
             {
                 name: "X",
-                username: "guritso",
                 url: "https://x.com/guritso",
             },
             {
                 name: "Github",
-                username: "Guritso",
                 url: "https://github.com/Guritso",
             },
             {
                 name: "MyAnimeList",
-                username: "Guritso",
                 url: "https://myanimelist.net/profile/Guritso",
             },
         ],
@@ -30,8 +29,8 @@ export async function GET({ url }) {
             rowSpeed: 0.6
         },
         repos: {
-            fork: false,
-            archived: false,
+            exclude: ["GuriTso", "gurizenit-site"],
+            excludeStatus: ["archived", "fork"],
         }
     }
   });
