595b684a1766764155739cfd52bf983d2509405c
GuriZenit <gurizenit@gmail.com>
2025-03-27 22:47:17 -0300

    chore(layout): format and align

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,30 +1,30 @@
 <script>
-    import "@fontsource-variable/m-plus-1-code";
+	import "@fontsource-variable/m-plus-1-code";
 </script>
 
 <slot />
 
 <style>
-    :global(html) {
-	margin: 0;
-	padding: 0;
-    }
+	:global(html) {
+		margin: 0;
+		padding: 0;
+	}
 
-    :global(body) {
+	:global(body) {
         background-color: #181a1b;
-	color: #e1dfdbcf;
-	font-family: "M PLUS 1 Code Variable", sans-serif;
-	font-optical-sizing: auto;
-	margin: 0;
-	padding: 0;
-	width: 100%;
-	height: 100%;
-	display: flex;
-	justify-content: center;
-	align-items: center;
-    }
-    :global(a) {
-        text-decoration: none;
-        color: #e1dfdbcf;
-    }
+		color: #e1dfdbcf;
+		font-family: "M PLUS 1 Code Variable", sans-serif;
+		font-optical-sizing: auto;
+		margin: 0;
+		padding: 0;
+		width: 100%;
+		height: 100%;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+	:global(a) {
+		text-decoration: none;
+		color: #e1dfdbcf;
+	}
 </style>
